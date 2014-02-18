b409fd4e9f04ed2a557d46ffb8cdc8eecb70a58e
Thomas Groshong <thomas.groshong@gmail.com>
2014-02-17 21:21:31 -0700

    Setup frontend Application Prototype.

--- software/applications/pc/HelloIOIOServlet/web/js/home.js
+++ software/applications/pc/HelloIOIOServlet/web/js/home.js
@@ -1,8 +1,52 @@
 console.log("JavaScript Running!");
+// var element = document.getElementById("js-test");
+// if(element){
+// 	element.textContent = "I executed external JavaScript!";
+// }
+// $('#js-test').css("color", "#333333");
 
-var element = document.getElementById("js-test");
-if(element){
-	element.textContent = "I executed external JavaScript!";
+
+var App = function () {
+    // Constructor
+    this.init();
 }
 
-$('#js-test').css("color", "#333333");
+App.prototype = {
+
+    init: function () {
+        console.log("App Running!");
+        this.configXML = this.getConfigXML();
+        this.setupTable();
+    },
+
+    getConfigXML: function() {
+        var xmlConfig = null
+        $.get( "/ioioconfig?resource=config.xml", function( xml ) {
+            var xmlObj = $( xml );
+            xmlConfig = xmlObj;
+
+            // Test XML Doc
+            console.log(xmlObj);
+            xmlObj.find("pin").each(function(index, value){
+                console.log(index, value.getAttribute("type"));
+            });
+        });
+        return xmlConfig;
+    },
+
+    setupTable: function(){
+        // Loop through XML config Doc and create the table for 
+        console.log("TODO: Create the config table.");
+    },
+
+    saveConfig: function(){
+        // Loop through config table rows, generate XML doc, POST to server
+        console.log("TODO: Build/Update XML doc and POST to server.");
+    },
+
+    attachHandlers: function(){
+        // Attach Event handlers to Save new Config
+        console.log("TODO: Attach event handlers to save XML");
+    },
+}
+
