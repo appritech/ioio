547fbf8ed84d040e556dd71d87a4494072042293
Thomas Groshong <thomas.groshong@gmail.com>
2014-03-02 11:14:54 -0700

    JavaScript work to parse retrieved XML data into a HTML table.

--- software/applications/pc/HelloIOIOServlet/web/js/home.js
+++ software/applications/pc/HelloIOIOServlet/web/js/home.js
@@ -1,42 +1,93 @@
 console.log("JavaScript Running!");
-// var element = document.getElementById("js-test");
-// if(element){
-// 	element.textContent = "I executed external JavaScript!";
-// }
-// $('#js-test').css("color", "#333333");
 
 
-var App = function () {
+
+var IOIOApp = function () {
     // Constructor
     this.init();
 }
 
-App.prototype = {
+IOIOApp.prototype = {
+    doutSubtypes: {
+        "OD": "Open-Drain",
+        "NL": "Normal"
+    },
+
+    dinSubtypes: {
+        "FL": "Float",
+        "PD": "Pull-Down",
+        "PU": "Pull-Up",
+    },
+
+    pinTypes: {
+        "ain": "Analog In",
+        "dout": "Digital Out",
+        "din": "Digital In"
+    },
+
+    analogValues: {
+        "MinInput": 0,
+        "MaxInput": 3.28676,
+        "CenterInput": 1.6976545,
+        "MinOutput": -100,
+        "MaxOutput": 100,
+        "CenterOutput": 0
+    },
 
     init: function () {
         console.log("App Running!");
-        this.configXML = this.getConfigXML();
-        this.setupTable();
+        this.getConfigXML();
+
     },
 
     getConfigXML: function() {
+        var self = this; 
         var xmlConfig = null
-        $.get( "/ioioconfig?resource=config.xml", function( xml ) {
-            var xmlObj = $( xml );
-            xmlConfig = xmlObj;
-
-            // Test XML Doc
-            console.log(xmlObj);
-            xmlObj.find("pin").each(function(index, value){
-                console.log(index, value.getAttribute("type"));
-            });
+        $.get( "/api/config?resource=config.xml", function( xml ) {
+            self.configXML = $( xml );
+            self.setupTable();
         });
-        return xmlConfig;
+
     },
 
     setupTable: function(){
         // Loop through XML config Doc and create the table for 
-        console.log("TODO: Create the config table.");
+        var strBuffer = [];
+        var self = this;
+        self.pinRowStore = [];
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/element
+        self.configXML.find("pin").each(function(index, pin){
+            var pinRow = new self.PinRow(self, pin);
+            self.pinRowStore.push(pinRow);
+
+            strBuffer.push("<tr>");
+            
+            // Pin Number Cell
+            strBuffer.push("<td>");
+            strBuffer.push(pinRow.number);
+            strBuffer.push("</td>");
+
+            // Pin Type Cell
+            strBuffer.push("<td>");
+            strBuffer.push(pinRow.getTypeSelect());
+            strBuffer.push("</td>");
+
+            // Pin Subtype Cell
+            strBuffer.push("<td>");
+            strBuffer.push(pinRow.getSubtypeSelect());
+            strBuffer.push("</td>");
+
+            // Pin I/O Status cell
+            strBuffer.push("<td>");
+            strBuffer.push(pinRow.getIOStatusHTML());
+            strBuffer.push("</td>");
+            
+            strBuffer.push("</tr>");
+        });
+
+        this.tableContents = strBuffer.join("");
+        $("#ioio-table-body").html(this.tableContents);
     },
 
     saveConfig: function(){
@@ -48,5 +99,84 @@ App.prototype = {
         // Attach Event handlers to Save new Config
         console.log("TODO: Attach event handlers to save XML");
     },
+    
+    PinRow: function (app, node){
+
+        this.node = node;
+        this.number = node.getAttribute("num");
+        this.typeAbbr = node.getAttribute("type");
+        this.type = app.pinTypes[this.typeAbbr];
+        this.subtypeAbbr = (node.hasAttribute("subtype"))? node.getAttribute("subtype"): null;
+        this.subtype = null;
+
+        if (this.typeAbbr == "dout"){
+            this.subtypeMap = app.doutSubtypes;
+        } else if (this.typeAbbr == "din"){
+            this.subtypeMap = app.dinSubtypes;
+        }
+
+        if (this.subtypeMap){
+            this.subtype = this.subtypeMap[this.subtypeAbbr];
+        }
+        
+        this.getTypeSelect = function(){
+            var htmlBuffer = [];
+            htmlBuffer.push("<select>");
+            for (var key in app.pinTypes){
+                // check hasOwnProperty
+                if (!app.pinTypes.hasOwnProperty(key)){
+                    continue;
+                }
+
+                var curType = app.pinTypes[key];
+                htmlBuffer.push("<option ")
+                if (curType == this.type){
+                    htmlBuffer.push("selected='selected'");
+                }
+                htmlBuffer.push(">");
+                htmlBuffer.push(curType);
+                htmlBuffer.push("</option>");
+            }
+            htmlBuffer.push("</select>");
+            return htmlBuffer.join("");
+        }
+
+        this.getSubtypeSelect = function(){
+            if (this.typeAbbr == 'ain'){
+                return ""
+            }
+            var htmlBuffer = [];
+            htmlBuffer.push("<select>");
+
+            for (var key in this.subtypeMap){
+                // check hasOwnProperty
+                if (!this.subtypeMap.hasOwnProperty(key)){
+                    continue;
+                }
+
+                var curSubtype = this.subtypeMap[key];
+
+                htmlBuffer.push("<option ")
+                if (curSubtype == this.subtype){
+                    htmlBuffer.push("selected='selected'");
+                }
+                htmlBuffer.push(">");
+                htmlBuffer.push(curSubtype);
+                htmlBuffer.push("</option>");
+            }
+            htmlBuffer.push("</select>");
+            return htmlBuffer.join("");
+        }
+
+        this.getIOStatusHTML = function(){
+            if (this.typeAbbr == "ain"){
+                return "<input type='checkbox' />"
+            }
+            return "<span class='label label-warning'>Stats</span>"
+        }
+    }
+
 }
 
+
+var ioapp = new IOIOApp;
